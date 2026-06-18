b7463f6d7233eb8234223ab9158ff32ffbf764f5
mainHossein <ho3eincheraghi20.06@gmail.com>
2026-06-18 21:50:04 +0330

    add swagger configuration

--- src/main/java/com/example/userserviceproject/UserServiceProjectApplication.java
+++ src/main/java/com/example/userserviceproject/UserServiceProjectApplication.java
@@ -3,13 +3,28 @@ package com.example.userserviceproject;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.core.env.ConfigurableEnvironment;
 
 @EnableCaching
 @SpringBootApplication
 public class UserServiceProjectApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(UserServiceProjectApplication.class, args);
+        ConfigurableApplicationContext run = SpringApplication.run(UserServiceProjectApplication.class, args);
+        ConfigurableEnvironment environment = run.getEnvironment();
+        Integer port = environment.getProperty("server.port", Integer.class);
+        String path = environment.getProperty("server.servlet.context-path");
+        printServerInfo(port, path);
+    }
+
+    private static void printServerInfo(Integer port, String path) {
+        System.out.printf("""
+                --------------------------------------------------
+                | Server URL: http://localhost:%d              |
+                | Swagger URL: http://localhost:%d%s/swagger-ui/index.html  |
+                --------------------------------------------------
+                %n""", port, port, path);
     }
 
 }
